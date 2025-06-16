dc44bda3ad6a7f2c26b39e00233944d2f9c6fdb8
Pamela Carriel <carrielpame@gmail.com>
2025-06-15 20:49:38 -0500

    update

--- Proyecto/index.php
+++ Proyecto/index.php
@@ -44,7 +44,7 @@ session_start();
     </header>
     <nav>
         <div class="nav-container">
-            <a href="#" class="has-dropdown">ACCESORIOS</a>
+            <a href="Productos/Accesorios/Accesorios.html" class="has-dropdown">ACCESORIOS</a>
             <div class="dropdown-menu">
                 <div class="dropdown-content">
                     <div class="dropdown-column">
@@ -198,7 +198,7 @@ session_start();
         <a href="Catalogo/catalogo.html" class="catalogo">CATÁLOGO</a>
     </nav>
 </div> <!-- Cierre de sticky-wrapper -->
-
+<br>
     <main>
         <!-- Carrusel de Promociones Mejorado -->
         <div class="promo-carousel-container">
@@ -252,7 +252,7 @@ session_start();
             </div>
         </div>
 
-        <!--=============================
+<!--=============================
 Seccion de productos
 /* ============================= */-->
         <section class="seccion-productos">
