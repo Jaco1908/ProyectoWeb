12e85fd3914ec55a8e92dcc203f826f1ae6aff4c
Pamela Carriel <carrielpame@gmail.com>
2025-06-23 09:48:44 -0500

    Quitarcomentario

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="es">
-<!--HOLA prueba-->
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
